408492bab4d4e34b31bbc378196ce7748815e8f5
Nathan <nembery@gmail.com>
2019-06-27 10:34:30 -0400

    add panos type and icon

---
+++ common/static/js/vm_types/panos.js
@@ -0,0 +1,16 @@
+draw2d.shape.node.panos = draw2d.shape.node.linux.extend({
+    NAME: "draw2d.shape.node.panos",
+    CLOUD_INIT_SUPPORT: true,
+    RESIZE_SUPPORT: false,
+    MANAGEMENT_INTERFACE_PREFIX: "management",
+    MANAGEMENT_INTERFACE_INDEX: 0,
+    INTERFACE_PREFIX: "ethernet0/",
+    INTERFACE_OFFSET: 0,
+    VCPU: 2,
+    VRAM: 8192,
+    ICON_WIDTH: 50,
+    ICON_HEIGHT: 50,
+    ICON_FILE: "/static/images/panos.png",
+
+});
+
