e90b89a2fa9599931735da2230dc7abdf017adba
Nathan <nembery@gmail.com>
2019-10-29 09:12:18 -0400

    add ubuntu 18 type explicitly

---
+++ common/static/js/vm_types/ubuntu18.js
@@ -0,0 +1,8 @@
+draw2d.shape.node.ubuntu16 = draw2d.shape.node.linux.extend({
+    NAME: "draw2d.shape.node.ubuntu16",
+    INTERFACE_PREFIX: "ens",
+    // useful for interface naming in the GUI and via automation actions
+    INTERFACE_OFFSET: 4,
+    MANAGEMENT_INTERFACE_PREFIX: "ens",
+    RESIZE_SUPPORT: true
+});
